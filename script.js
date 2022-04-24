bf240375632639e47babff9e47f4a4d1487a10dc
humanrizki <humanrizki123@gmail.com>
2022-04-24 21:21:09 +0700

    init 2

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let id = '#article1';
-document.querySelectorAll('.navbar .nav-ul > li').forEach(function(value, index){
+// console.log(document.getElementsByTagName('.navbar .nav-ul li'))[0]
+document.querySelectorAll('.navbar .nav-ul a').forEach(function(value, index){
     value.addEventListener('click',function(e){
+        console.log(e.target.getAttribute('href'))
         if(id != e.target.getAttribute('href')){
-            document.querySelector(`.nav-ul li a[href='${id}']`).parentNode.classList.toggle('bor-bot')
+            document.querySelector(`.nav-ul a[href='${id}']`).classList.toggle('bor-bot')
             id = e.target.getAttribute('href');
-            document.querySelector(`.nav-ul li a[href='${id}']`).parentNode.classList.toggle('bor-bot')
-            console.log(document.querySelector(`.nav-ul li a[href='${id}']`))
+            document.querySelector(`.nav-ul a[href='${id}']`).classList.toggle('bor-bot')
+            console.log(document.querySelector(`.nav-ul a[href='${id}']`))
         } else {
             id = e.target.getAttribute('href')
         }
     })
 })
-document.querySelector(`.nav-ul li a[href='${id}']`).parentNode.classList.toggle('bor-bot')
-document.querySelector(`${id.substring(1,id.length)}`).style.display = 'block'
+console.log(document.querySelector(`.nav-ul a[href='#${id}']`))
+// document.querySelector('tab-content:first-child').style.display = 'block'
+console.log(id.substring(1, id.length))
